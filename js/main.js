b931a8b0cc0ed5da061c095ec091ddcff3b5a477
dorshichek <pavluchenko96.org@mail.ru>
2022-04-26 22:40:24 +0300

    add template

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ UI_ELEMENTS.SETTINGS.addEventListener('click', openModal)
 
 UI_ELEMENTS.BUTTON_CLOSE.addEventListener('click', closeModal)
 
+UI_ELEMENTS.BUTTON_SEND_MESSAGE.addEventListener('click', sendMessage)
+
 function openModal() {
   UI_ELEMENTS.MODAL.classList.add('modal-active')
 }
@@ -12,7 +14,12 @@ function closeModal() {
   UI_ELEMENTS.MODAL.classList.remove('modal-active')
 }
 
-function showTime() {
-  UI_ELEMENTS.TIME.textContent = new Date()
+function sendMessage() {
+  const date = new Date
+  const element = document.createElement('div')
+  element.append(UI_ELEMENTS.MESSAGE.content.cloneNode(true))
+  element.querySelector('.message__text').textContent = 'Я:' + UI_ELEMENTS.INPUT_MESSAGE.value
+  element.querySelector('.message__time').textContent = date.getHours() + ':' + date.getMinutes()
+  UI_ELEMENTS.CHAT.append(element)
 }
-showTime()
+
--- js/view.js
+++ js/view.js
@@ -7,5 +7,11 @@ const UI_ELEMENTS = {
   BUTTON_CLOSE: document.querySelector('.modal__close'),
   MODAL: document.querySelector('.modal'),
   TIME: document.querySelector('.message__time'),
-  MESSAGE: document.querySelector('.message__text'),
+  MESSAGE: document.querySelector('.message'),
+  BUTTON_SEND_MESSAGE: document.querySelector('.form__send-message'),
+  INPUT_MESSAGE: document.querySelector('.form__message'),
+  MESSAGE_TEXT: document.querySelector('.message__text'),
+  TEMPLATE_MESSAGE: document.querySelector('.message'),
+  MESSAGE_BODY: document.querySelector('.message__body'),
+  CHAT: document.querySelector('.chat__wrapper')
 }
